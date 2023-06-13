3923672f527b09b5e4ccc86142bba8223ec563ac
DarkkneZPanda <ortegaluis1114@gmail.com>
2023-06-13 00:03:38 -0700

    Walk in progress

---
+++ CatWalk/Assets/Patrol.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+
+    public Transform[] moveSpots;
+
+    private int randomSpot;
+
+    void Start() {
+        
+    }
+
+}
